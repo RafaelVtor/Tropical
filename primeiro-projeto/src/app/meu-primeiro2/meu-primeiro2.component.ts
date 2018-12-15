bf56a1be2b37905dbb05570496c7e1781bd47ebe
Rafael Alves <rafaelvitor_as@hotmail.com>
2018-12-15 16:01:16 -0300

    Estudos Angular 15-12-2018

--- primeiro-projeto/src/app/meu-primeiro2/meu-primeiro2.component.ts
+++ primeiro-projeto/src/app/meu-primeiro2/meu-primeiro2.component.ts
@@ -7,7 +7,49 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MeuPrimeiro2Component implements OnInit {
 
-  constructor() { }
+  url: string = 'http://webpet.ga';
+  urlImagem = 'https://picsum.photos/200/300/?random';
+  cursoAngular: boolean = true;
+
+  valorAtual: string = '';
+  valorSalvo: string = '';
+
+  isMouseOver: boolean = false;
+
+  nome:string ='abc';
+
+  pessoa: any = {
+    nome:'def',
+    idade: 30
+  }
+
+  getCurtirCurso(){
+    return true;
+  }
+
+  getValor(){
+    return 1;
+  }
+
+  botaoClicado(){
+    alert("ola mundo!");
+  }
+  onKeyUp(evento , KeyboardEvent){
+    console.log((<HTMLInputElement>evento.target).value);
+    this.valorAtual = (<HTMLInputElement>evento.target).value;
+    
+  }
+  salvarValor(valor){
+    this.valorSalvo = valor;
+  }
+
+  onMouseOverOut(){
+    this.isMouseOver = !this.isMouseOver;
+
+  }
+  constructor() { 
+   
+  }
 
   ngOnInit() {
   }
---
+++ tropical-bordados/js/animacao.js
@@ -0,0 +1,20 @@
+
+
+const target = document.querySelectorAll('[data-anime]');
+const animationClass= 'animate';
+
+function animeScroll(){
+    
+    const windowTop = window.pageYOffset;
+  
+    target.forEach(function(element){
+        if ((windowTop) > element.offsetTop) {
+            element.classList.add(animationClass);            
+        }
+    })
+    
+}
+animeScroll();
+window.addEventListener('scroll', function(){
+    animeScroll();
+})
