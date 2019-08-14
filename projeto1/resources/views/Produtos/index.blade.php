cea507a5b516bdb2d4dfd9e81e5bee1a81760dca
Rafael Alves <rafaelvitor_as@hotmail.com>
2019-08-14 17:30:18 -0300

    projeto em laravel

---
+++ projeto1/resources/views/Produtos/index.blade.php
@@ -0,0 +1,45 @@
+@extends('layouts.app')
+@section('title', 'Lista de Produtos')
+@section('content')
+	<h1>Produtos</h1>
+	@if($message = Session::get('success'))
+		<div class="alert alert-success">
+			{{$message}}
+		</div>
+	@endif
+	<div>
+		<div class="col-md-12">
+			<form method="POST" action="{{url('produtos/busca')}}">
+				@csrf
+				<div class="input-group mb-3">
+					<input type="text" class="form-control" name="busca" id="busca" placeholder="Procurar produto no site..." value="{{$buscar}}">
+					<div class="input-group-append">
+						<button class="btn btn-outline-secondary">Buscar</button>
+					</div>
+				</div>				
+			</form>
+		</div>
+	</div>
+	<div class="row">
+		@foreach ($produtos as $produto)
+		<div class="col-md-3">
+			@if(file_exists("./img/produtos/".md5($produto->id).".jpg"))			
+				<img src="{{url('img/produtos/'.md5($produto->id).'.jpg')}}" alt="Imagem Produto" class="img-fluid img-thumbnail">			
+			@endif
+			<h4 class="text-center"><a href="{{URL::to('produtos')}}/{{$produto->id}}">{{$produto->titulo}}</h4>
+
+			@if(Auth::check())
+			<div>
+				<form method="POST" action="{{action('ProdutosController@destroy', $produto->id)}}">
+					@csrf
+					<input type="hidden" name="_method" value="DELETE">
+					<a href="{{URL::to('produtos/'.$produto->id.'/edit')}}" class="btn btn-primary">Editar</a>
+					<button class="btn btn-danger">Excluir</button>
+				</form>
+			</div>
+			@endif
+		</div>		
+		@endforeach
+	</div>
+	{{$produtos->links()}}
+@endsection
---
+++ projeto1/resources/views/contato.php
@@ -0,0 +1,2 @@
+<h1><?php echo $titulo ?></h1>
+<a href="rafa:rafa@hotmail.com">Envie uma mensagem</a>
